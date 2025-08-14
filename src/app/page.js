2059e5e941e31f67ed64f8fc65f5b2edd8f14ce0
lucian-xinitrc <cusmirlucian@gmail.com>
2025-08-14 18:43:55 +0000

    First Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,76 @@
+'use client'
 import Image from "next/image";
+import CryptoJS from "crypto-js";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [input, setInput] = useState("");
+  const [resultmd5, setResultmd5] = useState("");
+  const [resultsha1, setResultsha1] = useState("");
+  const [resultripemd160, setResultripemd160] = useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const md5func = () => {
+    const md5 = CryptoJS.MD5(input).toString();
+    const sha1 = CryptoJS.SHA1(input);
+    const ripemd160 = CryptoJS.RIPEMD160(input).toString();
+    setResultmd5(`${md5}`);
+    setResultsha1(`${sha1}`);
+    setResultripemd160(`${ripemd160}`)
+  };
+  return (
+    <> 
+      <section className="bg-white dark:bg-gray-950 ">
+        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-4 xl:gap-0 lg:py-16 lg:grid-cols-12 h-screen">
+            <div className="mr-auto place-self-center lg:col-span-8 w-full">
+              <h1 className="max-w-2xl mb-4 text-center sm:text-left text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Welcome to our Encryption Tool</h1>
+              <p className="max-w-2xl mb-6 font-light text-center sm:text-left text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Simply type your string below</p>
+                
+
+              <div className="max-w-sm mx-5">
+                <div className="mb-5 flex justify-center font-bold">
+                  <input 
+                    type="text" 
+                    id="email"
+                    onChange={(e) => setInput(e.target.value)} 
+                    className="rounded-full focus:outline-none border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-3xs sm:w-4xl md:w-[300px]" placeholder="Type your string here" required />
+                  <button 
+                    type="submit"
+                    onClick={md5func} 
+                    className="md:mx-5 sm:mx-3 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-auto sm:w-2xl md:w-[158px]">Submit</button>
+                </div>
+              </div>
+              <dl className="block sm:hidden max-w-md text-gray-900 place-self-center divide-y divide-gray-200 dark:text-white dark:divide-gray-700 lg:w-4xl">
+                <div className="flex flex-col pb-3">
+                    <dt className="mb-1 text-gray-500 md:text-lg dark:text-gray-400">MD5</dt>
+                    <dd className="text-lg font-semibold overflow-x-scroll w-3xs py-5">{resultmd5 && <p style={{ marginTop: "20px" }}>{resultmd5}</p>}</dd>
+                </div>
+                <div className="flex flex-col py-3">
+                    <dt className="mb-1 text-gray-500 md:text-lg dark:text-gray-400">SHA1</dt>
+                    <dd className="text-lg font-semibold overflow-x-scroll w-3xs py-5">{resultsha1 && <p style={{ marginTop: "20px" }}>{resultsha1}</p>}</dd>
+                </div>
+                <div className="flex flex-col pt-3">
+                    <dt className="mb-1 text-gray-500 md:text-lg dark:text-gray-400">RIPEMD-160</dt>
+                    <dd className="text-lg font-semibold overflow-x-scroll w-3xs py-5">{resultripemd160 && <p style={{ marginTop: "20px" }}>{resultripemd160}</p>}</dd>
+                </div>
+              </dl>
+            </div>
+            <dl className="hidden sm:block max-w-md text-gray-900 place-self-center divide-y divide-gray-200 dark:text-white dark:divide-gray-700 lg:w-4xl">
+                <div className="flex flex-col pb-3">
+                    <dt className="mb-1 text-gray-500 md:text-lg dark:text-gray-400">MD5</dt>
+                    <dd className="text-lg font-semibold py-5">{resultmd5 && <p style={{ marginTop: "20px" }}>{resultmd5}</p>}</dd>
+                </div>
+                <div className="flex flex-col py-3">
+                    <dt className="mb-1 text-gray-500 md:text-lg dark:text-gray-400">SHA1</dt>
+                    <dd className="text-lg font-semibold py-5">{resultsha1 && <p style={{ marginTop: "20px" }}>{resultsha1}</p>}</dd>
+                </div>
+                <div className="flex flex-col pt-3">
+                    <dt className="mb-1 text-gray-500 md:text-lg dark:text-gray-400">RIPEMD-160</dt>
+                    <dd className="text-lg font-semibold py-5">{resultripemd160 && <p style={{ marginTop: "20px" }}>{resultripemd160}</p>}</dd>
+                </div>
+              </dl>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+    </section>
+    </>
   );
 }
